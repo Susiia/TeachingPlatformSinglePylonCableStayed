7fa1822632f2df9b5ecbb2430678b81c1415d9ed
刘译蓬 <1-liuyipeng@users.noreply.gitlab.example.com>
2025-05-09 14:32:11 +0800

    refactor: 优化代码格式和注释，提高可读性

--- src/main.ts
+++ src/main.ts
@@ -25,13 +25,11 @@ import { SSAOPass } from "three/addons/postprocessing/SSAOPass.js";
 import { OutputPass } from "three/addons/postprocessing/OutputPass.js";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 import { FXAAShader } from "three/addons/shaders/FXAAShader.js";
-import { Water } from 'three/addons/objects/Water.js';
-
-
+import { Water } from "three/addons/objects/Water.js";
 
 // 创建场景
 const scene = new Scene();
-scene.fog = new FogExp2(0xA3ADB7, 0.002);// 雾
+scene.fog = new FogExp2(0xa3adb7, 0.002); // 雾
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(update);
@@ -87,28 +85,26 @@ const texture = _TextureLoader.load("/public/Textures/SkyBox.jpg", () => {
 });
 
 // 创建海洋
-const waterGeometry = new PlaneGeometry( 10000, 10000 );
+const waterGeometry = new PlaneGeometry(10000, 10000);
 
-let water = new Water(
-  waterGeometry,
-  {
-    textureWidth: 512,
-    textureHeight: 512,
-    waterNormals: new TextureLoader().load( '/public/Textures/waternormals.jpg', function ( texture ) {
+let water = new Water(waterGeometry, {
+  textureWidth: 512,
+  textureHeight: 512,
+  waterNormals: new TextureLoader().load(
+    "/public/Textures/waternormals.jpg",
+    (texture) => {
       texture.wrapS = texture.wrapT = RepeatWrapping;
-    } ),
-    sunDirection: new Vector3(),
-    sunColor: 0xffffff,
-    waterColor: 0x19386b,
-    distortionScale: 3.7,
-    fog: scene.fog !== undefined
-  }
-);
-water.rotation.x = - Math.PI / 2;
+    }
+  ),
+  sunDirection: new Vector3(),
+  sunColor: 0xffffff,
+  waterColor: 0x19386b,
+  distortionScale: 3.7,
+  fog: scene.fog !== undefined,
+});
+water.rotation.x = -Math.PI / 2;
 
-scene.add( water );
-
-
+scene.add(water);
 
 // 声明海洋材质
 let oceanMaterial: MeshStandardMaterial | undefined;
@@ -117,8 +113,8 @@ let oceanMaterial: MeshStandardMaterial | undefined;
 const _GLTFLoader = new GLTFLoader();
 _GLTFLoader.load(
   "/public/Models/TeachingPlatformSinglePylonCableStayedModel.glb",
-  function (gltf) {
-    gltf.scene.traverse(function (child) {
+  (gltf) => {
+    gltf.scene.traverse((child) => {
       if (child instanceof Mesh && child.material.name === "Ocean") {
         oceanMaterial = child.material;
       }
@@ -126,13 +122,10 @@ _GLTFLoader.load(
     scene.add(gltf.scene);
   },
   undefined,
-  function (error) {
+  (error) => {
     console.error(error);
   }
 );
-
-
-
 
 // 创建后期处理合成器
 const composer = new EffectComposer(renderer);
@@ -187,6 +180,6 @@ function update() {
   }
   // 3js自带的水面
   if (water) {
-    water.material.uniforms['time'].value += 1.0 / 60.0;
+    water.material.uniforms["time"].value += 1.0 / 60.0;
   }
 }
